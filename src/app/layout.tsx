053378f53c61c88b9c04b1bd88efa3022c996d65
Alex Santos <devalexsantos@gmail.com>
2023-10-03 17:32:29 -0300

    Aditioned Analytics Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Play } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const play = Play({ subsets: ['latin'], weight: ['400', '700'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" className="scroll-smooth">
-      <body className={play.className}>{children}</body>
+      <body className={play.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
